45e14668940b83a964455b1aa6d861b1cdb389e8
Simon Willison <swillison@gmail.com>
2021-12-07 09:44:55 -0800

    Demonstrate --namespace incident for ca-fires, refs #30

--- demos/ca-fires.sh
+++ demos/ca-fires.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git-history file ca-fires.db ca-fires-history/incidents.json \
+  --namespace incident \
   --repo ca-fires-history \
   --id UniqueId \
   --convert 'json.loads(content)["Incidents"]'
