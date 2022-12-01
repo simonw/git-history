3de1a72b51c11070072eff6269ab47c253e734fd
Harald Wartig <hwartig@gmail.com>
2022-12-01 20:16:09 +0100

    Add subdirectory support.

--- tests/test_git_history.py
+++ tests/test_git_history.py
@@ -54,6 +54,23 @@ def make_repo(tmpdir):
         ),
         "utf-8",
     )
+    sub_dir = repo_dir / "dir"
+    sub_dir.mkdir()
+    (sub_dir / "items.json").write_text(
+        json.dumps(
+            [
+                {
+                    "product_id": 1,
+                    "name": "Gin",
+                },
+                {
+                    "product_id": 2,
+                    "name": "Tonic",
+                },
+            ]
+        ),
+        "utf-8",
+    )
     (repo_dir / "items-with-reserved-columns.json").write_text(
         json.dumps(
             [
@@ -102,6 +119,7 @@ def make_repo(tmpdir):
             "add",
             "incidents.json",
             "items.json",
+            "dir/items.json",
             "items-with-reserved-columns.json",
             "items-with-banned-columns.json",
             "trees.csv",
@@ -113,6 +131,25 @@ def make_repo(tmpdir):
     subprocess.call(git_commit + ["-m", "first"], cwd=str(repo_dir))
     subprocess.call(["git", "branch", "-m", "main"], cwd=str(repo_dir))
     (repo_dir / "items.json").write_text(
+        json.dumps(
+            [
+                {
+                    "product_id": 1,
+                    "name": "Gin",
+                },
+                {
+                    "product_id": 2,
+                    "name": "Tonic 2",
+                },
+                {
+                    "product_id": 3,
+                    "name": "Rum",
+                },
+            ]
+        ),
+        "utf-8",
+    )
+    (sub_dir / "items.json").write_text(
         json.dumps(
             [
                 {
@@ -202,6 +239,50 @@ def test_file_without_id(repo, tmpdir, namespace):
     db_path = str(tmpdir / "db.db")
     with runner.isolated_filesystem():
         options = ["file", db_path, str(repo / "items.json"), "--repo", str(repo)]
+        if namespace:
+            options += ["--namespace", namespace]
+        result = runner.invoke(cli, options)
+    assert result.exit_code == 0
+    db = sqlite_utils.Database(db_path)
+    assert db.schema == (
+        "CREATE TABLE [namespaces] (\n"
+        "   [id] INTEGER PRIMARY KEY,\n"
+        "   [name] TEXT\n"
+        ");\n"
+        "CREATE UNIQUE INDEX [idx_namespaces_name]\n"
+        "    ON [namespaces] ([name]);\n"
+        "CREATE TABLE [commits] (\n"
+        "   [id] INTEGER PRIMARY KEY,\n"
+        "   [namespace] INTEGER REFERENCES [namespaces]([id]),\n"
+        "   [hash] TEXT,\n"
+        "   [commit_at] TEXT\n"
+        ");\n"
+        "CREATE UNIQUE INDEX [idx_commits_namespace_hash]\n"
+        "    ON [commits] ([namespace], [hash]);\n"
+        "CREATE TABLE [{}] (\n".format(namespace or "item")
+        + "   [product_id] INTEGER,\n"
+        "   [name] TEXT,\n"
+        "   [_commit] INTEGER REFERENCES [commits]([id])\n"
+        ");"
+    )
+    assert db["commits"].count == 2
+    # Should have some duplicates
+    assert [
+        (r["product_id"], r["name"], r["_commit"]) for r in db[namespace or "item"].rows
+    ] == [
+        (1, "Gin", 1),
+        (2, "Tonic", 1),
+        (1, "Gin", 2),
+        (2, "Tonic 2", 2),
+        (3, "Rum", 2),
+    ]
+
+@pytest.mark.parametrize("namespace", (None, "custom"))
+def test_file_in_subdirectory(repo, tmpdir, namespace):
+    runner = CliRunner()
+    db_path = str(tmpdir / "db.db")
+    with runner.isolated_filesystem():
+        options = ["file", db_path, str(repo / "dir/items.json"), "--repo", str(repo)]
         if namespace:
             options += ["--namespace", namespace]
         result = runner.invoke(cli, options)
